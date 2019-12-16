2a67b4aa910c2bd5ba0138c6f9c0360bea0c4804
anapayanruiz <anapayan22@gmail.com>
2019-12-16 21:45:56 +0100

    WIP in react-card.js

---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+<footer className="footer">
+  {" "}
+  <small className="footer__small">Awesome profile-cards @2018 </small>
+  <img src="./assets/images/NaN-rojo.png" alt="NaN-logo" className="footer__nan-logo footer__logo" />
+  <img src="./assets/images/logo-adalab.png" alt="Adalab-logo" className="footer__logo" />
+</footer>;
