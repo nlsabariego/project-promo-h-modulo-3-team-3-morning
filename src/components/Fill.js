4fd225a8522944bc0210abba1a15a971f5640d66
Azahara Martinez <azaharamaga@gmail.com>
2019-12-18 22:57:17 +0100

    Fix image in the input component

--- src/components/Fill.js
+++ src/components/Fill.js
@@ -5,17 +5,6 @@ function Fill() {
   return (
     <div className="js-container">
       <InputComponent />
-
-      <label for="image" className="form__label">
-        Imagen de perfil
-      </label>
-      <div className="form__image">
-        <input type="file" className="form__file js__profile-upload-btn" name="file" id="img-selector"></input>
-        <button for="file" className="form__image--btn js__profile-trigger">
-          Añadir imagen
-        </button>
-        <img className="form__image--min js__profile-preview-img" />
-      </div>
     </div>
   );
 }
