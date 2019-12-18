9133335b2489d4d80a09c4e52f989bd1b2b634de
Azahara Martinez <azaharamaga@gmail.com>
2019-12-18 15:55:30 +0100

    Add landing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./stylesheets/_reset.scss";
-import App from "./components/App";
+import Landing from "./components/Landing";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Landing />, document.getElementById("root"));
