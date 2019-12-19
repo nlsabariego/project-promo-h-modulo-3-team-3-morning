4cd1387fd98c6bd30d2a9c89816d80378a192d03
crismmgg <cristinammgg@gmail.com>
2019-12-19 13:49:10 +0100

    add collapsables event

--- src/components/Collapsable.js
+++ src/components/Collapsable.js
@@ -1,18 +1,42 @@
 import React from "react";
 
-function Collapsable(props) {
-  return (
-    <section className="design content-collapsible">
-      <div className="collapse js-collapsible__tigger">
-        <div className="collapse__items">
-          <i className={props.icon}></i>
-          <h1 className="collapse__items-title">{props.title}</h1>
+class Collapsable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      typeDisplay: "js-container"
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => {
+      let nextTypeDisplay;
+      if (prevState.typeDisplay === "js-container") {
+        nextTypeDisplay = "content-collapsible";
+      } else {
+        nextTypeDisplay = "js-container";
+      }
+      return {
+        typeDisplay: nextTypeDisplay
+      };
+    });
+  }
+
+  render() {
+    return (
+      <section className="design content-collapsible">
+        <div className={`collapse js-collapsible__tigger ${this.state.typeDisplay}`} onClick={this.handleClick}>
+          <div className="collapse__items">
+            <i className={this.props.icon}></i>
+            <h1 className="collapse__items-title">{this.props.title}</h1>
+          </div>
+          <i className="fas fa-angle-down collapse__icon-down"></i>
         </div>
-        <i className="fas fa-angle-down collapse__icon-down"></i>
-      </div>
-      {props.children}
-    </section>
-  );
+        {this.props.children}
+      </section>
+    );
+  }
 }
 
 export default Collapsable;
