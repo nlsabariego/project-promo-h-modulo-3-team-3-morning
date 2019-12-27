fd973691a0751c6bc7d295bd9e8987c632d42038
Azahara Martinez <azaharamaga@gmail.com>
2019-12-27 21:09:10 +0100

    Create reset component style

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "../stylesheets/Reset.scss";
 
 function Reset(props) {
   return (
     <button className="button--delete" onClick={props.handleReset}>
-      <i className="far fa-trash-alt trash__icon"></i>Reset
+      <i className="far fa-trash-alt"></i>Reset
     </button>
   );
 }
