639e19b203728d422c00ec5932d45ad0feb1aca1
Azahara Martinez <azaharamaga@gmail.com>
2020-01-12 12:18:51 +0100

    Show share results only when url is present

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../stylesheets/Share.scss";
 
-let twitterMsg = 'This is my awesome contact card!!';
+let twitterMsg = "This is my awesome contact card!!";
 
 function Share(props) {
   function handleFetch() {
@@ -10,24 +10,32 @@ function Share(props) {
     }
   }
   return (
-
     <div className="js-container">
       <div className="share">
         <button
           className={`share__button  js-button ${props.isValidated === true ? "share__button--active" : ""}`}
           type="submit"
           onClick={handleFetch}
-
         >
           <i className="far fa-address-card"></i>Crear tarjeta
-          </button>
-        <div className={`share__result 
-          ${props.isValidated === false ? "share__result--hidden" : ""}`}>
+        </button>
+        <div
+          className={`share__result 
+          ${props.url === "" ? "share__result--hidden" : ""}`}
+        >
           <p className="share__result__text">La tarjeta ha sido creada:</p>
-          <a className="share__result__url" href={props.url} rel="noopener noreferrer" target="_blank">{props.url} </a>
-          <a className="share__result__button" href={`https://twitter.com/intent/tweet?text=${twitterMsg} ${props.url}`} title="Comparte en twitter" rel="noopener noreferrer" target="_blank">
+          <a className="share__result__url" href={props.url} rel="noopener noreferrer" target="_blank">
+            {props.url}
+          </a>
+          <a
+            className="share__result__button"
+            href={`https://twitter.com/intent/tweet?text=${twitterMsg} ${props.url}`}
+            title="Comparte en twitter"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             <i className="fab fa-twitter"></i>Compartir en twitter
-            </a>
+          </a>
         </div>
       </div>
     </div>
